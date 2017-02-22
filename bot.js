f4e5476cc6bdc0bb4a24b3ef5f5fb3742a3742fb
Thomas Hirter <t.hirter@outlook.com>
2017-02-23 00:10:22 +0100

    AutobahnJS Example & more

--- bot.js
+++ bot.js
@@ -2,7 +2,14 @@
 
 var config = require('./config.js');
 const TeleBot = require('telebot');
-const bot = new TeleBot(config.API);
+const bot = new TeleBot({
+    token: config.API,
+    webhook: {
+        url: 'https://....',
+        host: '0.0.0.0',
+        port: 443
+    }
+});
 
 return bot.sendMessage(config.adminID, 'Test nachricht vom neuen node Bot');
 bot.connect();
---
+++ webhook.js
@@ -0,0 +1,30 @@
+var autobahn = require('autobahn');
+
+var connection = new autobahn.Connection({url: 'ws://127.0.0.1:9000/', realm: 'realm1'});
+
+connection.onopen = function (session) {
+
+    // 1) subscribe to a topic
+    function onevent(args) {
+        console.log("Event:", args[0]);
+    }
+    session.subscribe('com.myapp.hello', onevent);
+
+    // 2) publish an event
+    session.publish('com.myapp.hello', ['Hello, world!']);
+
+    // 3) register a procedure for remoting
+    function add2(args) {
+        return args[0] + args[1];
+    }
+    session.register('com.myapp.add2', add2);
+
+    // 4) call a remote procedure
+    session.call('com.myapp.add2', [2, 3]).then(
+        function (res) {
+            console.log("Result:", res);
+        }
+    );
+};
+
+connection.open();
