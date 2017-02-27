0f357c5ee7c5146b1f54a8412cfe8d9fb93cf767
Thomas Hirter <t.hirter@outlook.com>
2017-02-27 20:49:23 +0100

    Update sql

--- pokemonnotifier.sql
+++ pokemonnotifier.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 24. Feb 2017 um 17:18
+-- Erstellungszeit: 27. Feb 2017 um 20:48
 -- Server-Version: 10.1.19-MariaDB
 -- PHP-Version: 7.0.13
 
@@ -29,24 +29,12 @@ SET time_zone = "+00:00";
 CREATE TABLE `chats` (
   `chat_id` varchar(50) NOT NULL,
   `admin` int(11) DEFAULT '0',
-  `place` varchar(45) DEFAULT NULL,
-  `lat` varchar(50) NOT NULL,
-  `lon` varchar(50) NOT NULL,
-  `priority` int(11) DEFAULT NULL,
+  `lat` float NOT NULL,
+  `lon` float NOT NULL,
+  `radius` float NOT NULL DEFAULT '0',
   `chat_info` text,
   `active` int(1) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `invites`
---
-
-CREATE TABLE `invites` (
-  `code` varchar(10) NOT NULL,
-  `chat_id` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -79,12 +67,6 @@ CREATE TABLE `notify_pokemon` (
 -- Indizes für die Tabelle `chats`
 --
 ALTER TABLE `chats`
-  ADD PRIMARY KEY (`chat_id`);
-
---
--- Indizes für die Tabelle `invites`
---
-ALTER TABLE `invites`
   ADD PRIMARY KEY (`chat_id`);
 
 --
